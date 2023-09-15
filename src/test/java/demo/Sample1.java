5e6ebb529b1ad1cc376c77c641bcf7d9df3b6cfa
dell <dell@PC-Revathi>
2023-09-15 10:18:36 +0530

    TC_003 branch committed

--- src/test/java/demo/Sample1.java
+++ src/test/java/demo/Sample1.java
@@ -5,7 +5,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi,this is my first commit");
-
+		System.out.println("to create local  branch and do push");
 	}
 
 }
